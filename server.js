172fc7d33dabccf6d9aa36777f383e6a239d4597
swapnik <sw695101@dal.ca>
2025-07-04 22:40:12 -0300

    deploy to vercel

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api-docs", require("./swagger/swagger"));
 
+const PORT = process.env.PORT || 5000;
+
 // Connect DB and Start Server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch((err) => console.error("MongoDB connection failed:", err));
+
